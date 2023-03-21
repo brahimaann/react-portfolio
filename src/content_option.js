166c3b935040d33e00496cd98506f2dd7e734771
brahimaann <brahimaann3@gmail.com>
2023-03-21 17:01:18 +0000

    Initial commit

---
+++ src/content_option.js
@@ -0,0 +1,91 @@
+const logotext = "Brahima";
+const meta = {
+    title: "Brahima Ann",
+    description: "I’m Brahima a third-year computer science student,currently studying at University of St. Thomas",
+};
+
+const introdata = {
+    title: "I’m Brahima Ann",
+    animated: {
+        first: "I enjoy creative coding",
+        second: "I'm a third-year student",
+    },
+    description: "Currently a third-year computer science student studying at the University of St. Thomas. For more about me navigate to the About page!",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "About my self",
+    aboutme: "As a self-motivated and curious third-year university student pursuing a B.S. in Computer Science, I am highly organized and detail-oriented with strong analytical problem-solving skills. My ability to learn quickly and work hard drives me to bring practical and creative solutions to every team and project I am a part of.",
+};
+
+const skills = [{
+        name: "Python",
+        value: 85,
+    },
+    {
+        name: "Java",
+        value: 90,
+    },
+    {
+        name: "MIPs",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 60,
+    },
+    {
+        name: "HTML",
+        value: 85,
+    },
+    {
+        name: "Javascript",
+        value: 75,
+    },
+    {
+        name: "CSS",
+        value: 80,
+    },
+    {
+        name: "C#",
+        value: 65,
+    },
+];
+
+const services = [{
+        title: "Digital Assistant",
+        description: "Developing a sophisticated digital assistant program that will analyze microphone input, transform it into a structured format for evaluation, and execute the desired task with precision.",
+    },
+    {
+        title: "Image Processor",
+        description: "image editing program developed using Java that allows for the manipulation of various aspects of an image to achieve desired results.",
+    },
+
+];
+
+
+
+const contactConfig = {
+    YOUR_EMAIL: "brahimaann3@gmail.com",
+    YOUR_FONE: "(763)445-1895",
+    description: "Please don't hesitate to contact me!",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_gyxp52w",
+    YOUR_TEMPLATE_ID: "template_8jhktgq",
+    YOUR_USER_ID: "HUB_5RvPq2UosXI4B",
+};
+
+const socialprofils = {
+};
+export {
+    meta,
+    dataabout,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
